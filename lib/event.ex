fe2299d29afa741b055cee5b2d0d202414e749c1
Rafael Carneiro <rafaelmdcarneiro@gmail.com>
2024-05-27 23:34:24 -0300

    Add files via upload

---
+++ lib/event.ex
@@ -0,0 +1,184 @@
+defmodule IRC.Event do
+  use GenEvent
+
+  def handle_event({ event, parts, client }, messages) do
+    { :ok, [{event, parts, client} | messages ]}
+  end
+
+  def handle_call(:messages, messages) do
+    { :ok, messages, [] }
+  end
+
+  def handle_events do
+    stream = GenEvent.stream(Events)
+    for event <- stream do
+      case event do
+        { "NICK", [nick], client } ->
+          handle_nick(client, nick)
+        { "CAP" , [whatever], client } ->
+          handle_cap(client, whatever)
+        { "JOIN", [channel], client } ->
+          handle_join(client, channel)
+        { "PART", [channel | part_message], client } ->
+          handle_part(client, channel, part_message)
+        { "MODE", [channel], client } ->
+          handle_mode(client, channel)
+        { "PRIVMSG", [channel | parts ], client } ->
+          handle_privmsg(client, channel, parts)
+        { "WHO", [channel], client } ->
+          handle_who(client, channel)
+        { "QUIT", parts, client } ->
+          handle_quit(client, ["QUIT" | parts])
+        { "KICK", [channel | parts], client } ->
+          handle_kick(client, channel, parts)
+        { "PING", [name], client } ->
+          handle_ping(client, name)
+        _ ->
+          IO.puts "Unhandled event!"
+          IO.inspect(event)
+      end
+    end
+  end
+
+  def reply(client, msg) do
+    IO.puts("-> #{msg}")
+    :gen_tcp.send(client, "#{msg}\r\n")
+  end
+
+  defp handle_nick(client, nick) do
+    event_user = lookup_user(client)
+    # Need to set ident here, as the reply needs to contain old nick
+    ident = event_user |> ident_for
+    Agent.update(Users, fn users -> Dict.put(users[client], :nick, nick) end)
+    msg = "#{ident} NICK #{nick}"
+    mass_broadcast_for(event_user, msg)
+  end
+
+  defp handle_cap(_socket, _msg) do
+    # TODO: WTF is CAP?
+  end
+
+  defp handle_join(client, channel) do
+    user = lookup_user(client)
+    ident = ident_for(user)
+
+    # Attempt to create the channel if it doesn't exist already.
+    create_new_channel(channel)
+    channel_data = Agent.get(Channels, fn channels -> channels[channel] end)
+    # User has joined channel, so add them to the list.
+    channel_users = [ client | channel_data.users ]
+    channel_data = Dict.put(channel_data, :users, channel_users)
+    Agent.update(Channels, fn channels -> Dict.put(channels, channel, channel_data) end)
+
+    # Add this channel to the list of channels for the user
+    user = Dict.put(user, :channels, [ channel | user.channels ])
+    Agent.update(Users, fn users -> Dict.put(users, client, user) end)
+
+    channel_broadcast(channel_users, "#{ident} JOIN #{channel}")
+
+    # Show the topic
+    reply(client, ":irc.localhost 332 #{user.nick} #{channel} :this is a topic and it is a grand topic")
+    # And a list of names
+    names = channel_data.users
+      |> Enum.map(fn (user) -> lookup_user(user).nick end)
+      |> Enum.join(" ")
+    reply(client, ":irc.localhost 353 #{user.nick} = #{channel} :#{names}")
+    reply(client, ":irc.localhost 366 #{user.nick} #{channel} :End of /NAMES list.")
+  end
+
+  defp handle_part(client, channel, part_message) do
+    user = lookup_user(client)
+    ident = ident_for(user)
+
+    channel_data = Agent.get(Channels, fn channels -> channels[channel] end)
+
+    part_message = Enum.join(part_message, " ")
+    channel_broadcast(channel_data.users, "#{ident} PART #{channel} #{part_message}")
+
+    # User has left the channel, so delete them from list.
+    users = Enum.reject(channel_data.users, fn (user) -> user == client end)
+    channel_data = Dict.put(channel_data, :users, users)
+    Agent.update(Channels, fn channels -> Dict.put(channels, channel, channel_data) end)
+  end
+
+  defp handle_mode(_socket, _channel) do
+    # TODO
+  end
+
+  defp handle_privmsg(client, channel, parts) do
+    create_new_channel(channel)
+    channel_data = lookup_channel(channel)
+    user = lookup_user(client)
+    ident = ident_for(user)
+    message = Enum.join(parts, " ") #|> String.slice(1..-1)
+    case Enum.any?(channel_data.users, fn (user) -> user == client end) do
+      true ->
+        users = Enum.reject(channel_data.users, fn (user) -> user == client end)
+        channel_broadcast(users, "#{ident} PRIVMSG #{channel} #{message}")
+      false ->
+      reply(client, ":irc.localhost 404 #{user.nick} #{channel} :Cannot send to channel")
+    end
+  end
+
+  defp handle_kick(client, channel, parts) do
+    channel_data = lookup_channel(channel)
+    user = lookup_user(client)
+    msg = "#{ident_for(user)} KICK #{channel} #{Enum.join(parts, " ")}"
+    channel_broadcast(channel_data.users, msg)
+  end
+
+  defp handle_who(_socket, _channel) do
+    # TODO: implement
+    # The IRC spec isn't helpful for this
+    # Probably best to check with a real IRC server and see its response to this command
+  end
+
+  defp handle_quit(client, parts) do
+    user = lookup_user(client)
+    msg = "#{ident_for(user)} #{Enum.join(parts, " ")}"
+    mass_broadcast_for(user, msg)
+    # TODO: Remove user from all channels they're a part of
+    Agent.update(Users, fn users -> Dict.delete(users, client) end)
+    # Commented out because it crashes the server!
+    # client.close
+  end
+
+  defp handle_ping(client, name) do
+    reply(client, "PONG #{name}")
+  end
+
+  # Used to broadcast events like QUIT or NICK.
+  defp mass_broadcast_for(event_user, msg) do
+    event_user.channels
+      |> Enum.each(
+        fn (channel) ->
+          Enum.each(lookup_channel(channel).users, fn (user) ->
+            reply(user, msg)
+          end)
+        end
+      )
+  end
+
+  defp channel_broadcast(users, message) do
+    Enum.each users, fn (user) ->
+      reply(user, message)
+    end
+  end
+
+  defp ident_for(user) do
+    username = String.slice(user.username, 0..7)
+    ":#{user.nick}!~#{username}@#{user.hostname}"
+  end
+
+  defp lookup_user(client) do
+    Agent.get(Users, fn users -> users[client] end)
+  end
+
+  defp lookup_channel(channel) do
+    Agent.get(Channels, fn channels -> channels[channel] end)
+  end
+
+  defp create_new_channel(channel) do
+    Agent.update(Channels, fn channels -> Dict.put_new(channels, channel, %{users: []}) end)
+  end
+end
---
+++ lib/server.ex
@@ -0,0 +1,110 @@
+defmodule IRC.Server do
+  import IRC.Event, only: [reply: 2]
+
+  def accept(port \\ 6667) do
+    GenEvent.start_link(name: Events)
+    GenEvent.add_handler(Events, IRC.Event, [])
+
+    Agent.start_link(fn -> %{} end, name: Users)
+    Agent.start_link(fn -> %{} end, name: Channels)
+
+    case :gen_tcp.listen(port, [:binary, packet: :line, active: false, reuseaddr: true]) do
+      { :ok, socket } ->
+        
+        Task.async(fn -> loop_acceptor(socket) end)
+        IO.puts "IRC Server started up and is now accepting requests."  
+
+        IRC.Event.handle_events
+      { :error, :eaddrinuse } ->
+        IO.puts "Address already in use"
+        System.halt(1)
+    end
+
+  end
+
+  defp loop_acceptor(socket) do
+    { :ok, client } = :gen_tcp.accept(socket)
+    {:ok, pid} = Task.Supervisor.start_child(IRC.Server.TaskSupervisor, fn -> initial_serve(client) end)
+    :ok = :gen_tcp.controlling_process(client, pid)
+    loop_acceptor(socket)
+  end
+
+  # Handles initial connection handshake with USER + NICK messages
+  defp initial_serve(client) do
+    case :gen_tcp.recv(client, 0) do
+      { :ok, data } ->
+        Agent.update(Users, fn users -> Dict.put_new(users, client, %{ channels: [] }) end)
+        data = String.strip(data) |> String.split(" ")
+        case data do
+          ["NICK" | nick] ->
+            hostname = resolve_hostname(client)
+            Agent.update(Users, fn users ->
+              user_data = users[client]
+              user_data = Dict.put(user_data, :nick, nick) 
+              user_data = Dict.put(user_data, :hostname, hostname) 
+              Dict.put(users, client, user_data)
+            end)
+          ["USER", username, _mode, _ | real_name_parts] ->
+            Agent.update(Users, fn users -> 
+              user_data = users[client]
+              user_data = Dict.put(user_data, :username, username) 
+              user_data = Dict.put(user_data, :real_name, Enum.join(real_name_parts, " "))
+              Dict.put(users, client, user_data) 
+            end)
+          other -> IO.inspect("Received unknown message: #{Enum.join(other, "")}")
+        end
+
+        user = Agent.get(Users, fn users -> users[client] end)
+        case user do
+          %{nick: nick, hostname: _, username: _, real_name: _ } ->
+            # User has connected and sent through NICK + USER messages.
+            # It's showtime!
+            welcome(client, nick)
+            serve(client)
+          _ ->
+            # User has not sent through all the right messages yet.
+            # Keep listening!
+            initial_serve(client)
+        end
+
+      { :error, :closed } ->
+        IO.puts "Connection closed by client."
+    end
+  end
+
+  defp serve(client) do
+    case :gen_tcp.recv(client, 0) do
+      { :ok, data } ->
+        IO.puts "<- #{String.strip(data)}"
+        String.strip(data) |> String.split(" ") |> process(client)
+        serve(client)
+      { :error, :closed } ->
+        IO.puts "Connection closed by client."
+    end
+  end
+
+  defp resolve_hostname(client) do
+    {:ok, {ip, _port}} = :inet.peername(client)
+    case :inet.gethostbyaddr(ip) do
+      { :ok, { :hostent, hostname, _, _, _, _}} ->
+        hostname
+      { :error, _error } -> 
+        IO.puts "Could not resolve hostname for #{ip}. Using IP instead."
+        Enum.join(Tuple.to_list(ip), ".")
+    end
+  end
+
+  defp welcome(client, nick) do
+    reply(client, ":irc.localhost 001 #{nick} Welcome to the IRC network.")
+    reply(client, ":irc.localhost 002 #{nick} Your host is exIRC, running version 0.0.1.")
+    reply(client, ":irc.localhost 003 #{nick} exIRC 0.0.1 +i +int")
+    reply(client, ":irc.localhost 422 :MOTD File is missing")
+  end
+
+
+  defp process([event | parts], client) do
+    GenEvent.notify(Events, { event, parts, client })
+  end
+
+end
+
